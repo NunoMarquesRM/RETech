4589d220ce9c80a2a4112ffbe4e6311651cd28e8
Nuno Marques <nuno_marques__1@hotmail.com>
2021-01-29 13:32:12 +0000

    Patch 0.3.0
    Added Satisfactory category

---
+++ prototypes/categories_Satisfactory.lua
@@ -0,0 +1,46 @@
+data:extend({
+	{--Group
+		type = "item-group",
+		name = "satisfactory-extends",
+		order = "daa-e",
+		inventory_order = "d-a-e",
+		icon = "__RETech__/graphics/item-group/satisfactory.png",
+		icon_size = 64,
+	},
+	{--Ingots
+		type = "item-subgroup",
+		name = "ingots",
+		group = "satisfactory-extends",
+		order = "a",
+	},
+	{--Iron
+		type = "item-subgroup",
+		name = "iron-extends",
+		group = "satisfactory-extends",
+		order = "b",
+	},
+	{--Copper
+		type = "item-subgroup",
+		name = "copper-extends",
+		group = "satisfactory-extends",
+		order = "c",
+	},
+	{--Concrete
+		type = "item-subgroup",
+		name = "concrete-extends",
+		group = "satisfactory-extends",
+		order = "d",
+	},
+	{--Tier 2
+		type = "item-subgroup",
+		name = "tier2-extends",
+		group = "satisfactory-extends",
+		order = "e",
+	},
+	--[[{--
+		type = "item-subgroup",
+		name = "",
+		group = "satisfactory-extends",
+		order = "",
+	},]]
+})
